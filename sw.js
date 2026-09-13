fc923e57743b8e921b4a80bad3678202cea743cf
好瞌睡的小白 <468562341@qq.com>
2026-09-13 22:17:38 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kaoyan-v2';
+const CACHE = 'kaoyan-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
